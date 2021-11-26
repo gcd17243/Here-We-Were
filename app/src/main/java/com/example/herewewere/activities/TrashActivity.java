194cbfe367a4bbc63686e7e3cf131dc539dde784
Nguyen Nam Ha <hanngcd17243>
2021-11-26 19:47:50 +0700

    Add Home page 11/26/2021

---
+++ app/src/main/java/com/example/herewewere/activities/TrashActivity.java
@@ -0,0 +1,16 @@
+package com.example.herewewere.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.herewewere.R;
+
+public class TrashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trash);
+    }
+}
